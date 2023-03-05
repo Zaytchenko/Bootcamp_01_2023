c14a06d7b00917024c58235514bc81f5c20d4ed7
@Zaytchenko <zaytchenko@gmail.com>
2023-03-05 23:20:32 +0200

    Format Document

--- questionnaire/Program.cs
+++ questionnaire/Program.cs
@@ -1,25 +1,19 @@
 ﻿// Написать программу, которая из имеющегося массива строк формирует массив строк, длина которых меньше или равна 3 символа.
 // Первоначальный массив надо ввести с клавиатуры, либо задать на старте выполнения алгоритма.
 
-
-
 System.Console.Write("Введите количество строк в исходном массиве:  ");
 int size = int.Parse(Console.ReadLine());
-// ввод строк
 string[] InputArray = new string[size];
-string[] OutputArray = new string[size];
+string[] TempArray = new string[size];
 for (int i = 0; i < size; i++)
 {
     System.Console.Write($"Введите {i} элемент массива:  ");
     InputArray[i] = Console.ReadLine();
     int lenght = InputArray[i].Length;
-     if (lenght < 4) 
-     OutputArray[i] = InputArray [i];
+    if (lenght < 4)
+        TempArray[i] = InputArray[i];
 }
-var array = OutputArray.Where(x => !string.IsNullOrWhiteSpace(x));
-
+var OutputArray = TempArray.Where(x => !string.IsNullOrWhiteSpace(x));
 Console.Clear();
-Console.WriteLine("Исходный массив [ " + string.Join(", ", InputArray)+" ]");
-Console.WriteLine("Полученный массив [ " + string.Join(", ", array)+" ]");
-
-
+Console.WriteLine("Исходный массив [ " + string.Join(", ", InputArray) + " ]");
+Console.WriteLine("Полученный массив [ " + string.Join(", ", OutputArray) + " ]");
